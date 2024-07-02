b329882be3907e4898d748da6f533b0a5fc08c2e
Adityya Kaushal <adityyakaushal@gmail.com>
2024-07-02 15:36:46 -0400

    started working on the home page

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,8 @@
+<nav class="w-screen h-10 flex justify-between items-center px-4 font-bold">
+	<div>Logo</div>
+	<ul class="list-none flex">
+		<li class="px-4">Hallo</li>
+		<li class="px-4">Hallo</li>
+	</ul>
+	<button>Resume</button>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-screen h-[95vh] grid md:grid-cols-2 md:grid-rows-1 grid-cols-1 grid-rows-2">
+	<div class="flex items-center p-[10%]">
+		<div class="text-center md:text-left">
+			<h1 class="text-5xl font-bold">Hello, my name is Adityya. 👋</h1>
+			<h2 class="text-2xl font-bold py-3">I am a student at the university of waterloo!</h2>
+			<button class="text-center text-xl bg-blue-300 p-2 rounded-xl">Contact Me</button>
+		</div>
+	</div>
+	<div class="flex justify-center items-center">Grid Element 2</div>
+</div>
+
