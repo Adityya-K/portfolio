2b02fc00ed4a9615370d2057a76dcd59f515c363
Adityya-K <adityyakaushal@gmail.com>
2024-12-17 12:47:39 -0500

    started main layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,42 @@
 </script>
 
 <div
+	style="transform:translateX({m.x - circle_size * 0.5}px) translateY({m.y -
+		circle_size * 0.5}px); width:{circle_size}px; height:{circle_size}px;"
+	class="fixed left-0 top-0 -z-20 bg-mouse-radial"
+></div>
+<div class="fixed left-0 top-0 -z-30 h-screen w-screen bg-[#00172D]"></div>
+
+<div
 	class="fixed left-0 top-0 h-screen w-screen text-white"
 	role="region"
 	onmousemove={handleMousemove}
-></div>
-<div
-	style="transform:translateX({m.x - circle_size * 0.5}px) translateY({m.y -
-		circle_size * 0.5}px); width:{circle_size}px; height:{circle_size}px;"
-	class="bg-mouse-radial fixed left-0 top-0 -z-20"
-></div>
-<div class="fixed left-0 top-0 -z-30 h-screen w-screen bg-[#00172D]"></div>
+>
+	<div class="flex w-screen justify-center">
+		<div class="grid w-full max-w-[1200px] pt-32 lg:grid-cols-2">
+			<div class="w-full pr-16 text-blue-100">
+				<h1 class="text-5xl font-bold">Adityya Kaushal</h1>
+				<h2 class="pt-4 text-2xl">
+					Mechatronics Undergraduate Student at the University of Waterloo
+				</h2>
+				<h3 class="pr-64 pt-5 text-lg opacity-75">
+					I like to explore the intersection of software and hardware!
+				</h3>
+			</div>
+			<div class="w-full">
+				<p class="text-lg leading-loose text-blue-200 opacity-75">
+					I am a university undergraduate student about the intersection between software and
+					hardware, particularily in the field of <b class="text-blue-50 opacity-100">robotics</b>.
+					It is my hope that I can augment great software like artificial intelligence with great
+					hardware like robots.
+				</p>
+				<p class="pt-4 text-lg leading-loose text-blue-200 opacity-75">
+					I am a university undergraduate student about the intersection between software and
+					hardware, particularily in the field of <b class="text-blue-50 opacity-100">robotics</b>.
+					It is my hope that I can augment great software like artificial intelligence with great
+					hardware like robots.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
