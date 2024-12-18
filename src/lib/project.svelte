b3a651603ac3a782deba2fe6bcaf422b6eccb480
Adityya-K <adityyakaushal@gmail.com>
2024-12-17 22:04:11 -0500

    finished project component

--- src/lib/project.svelte
+++ src/lib/project.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
+	let { year, body, title } = $props();
 </script>
 
-<div class="grid w-full grid-cols-4 pt-4">
+<div
+	class="grid w-full grid-cols-4 rounded-md border-solid px-4 py-4 text-blue-200 hover:bg-blue-300 hover:bg-opacity-5 hover:text-blue-400"
+>
 	<div class="col-span-1">
-		<h3 class="text-base text-blue-300 opacity-75">2021-2020</h3>
+		<h3 class="text-base text-blue-300 opacity-75">{year}</h3>
 	</div>
 	<div class="col-span-3">
-		<h1 class="text-xl font-bold text-blue-200">Title</h1>
-		<h2 class="text-base text-blue-200 opacity-75">
-			Hallo my name is Adityya and I have worked at very many places
-		</h2>
+		<h1 class="text-xl font-bold">{title}</h1>
+		<h2 class="text-base text-blue-200 opacity-75 hover:text-blue-200">{body}</h2>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,31 +16,41 @@
 <div class="fixed left-0 top-0 -z-30 h-screen w-screen bg-[#00172D]"></div>
 
 <div class="flex w-screen justify-center" onmousemove={handleMousemove} role="region">
-	<div class="grid w-full max-w-[1200px] pt-32 lg:grid-cols-2">
-		<div class="sticky top-32 w-full self-start pr-16 text-blue-100">
+	<div class="grid w-full max-w-[1200px] pt-32 lg:grid-cols-7">
+		<div class="sticky top-32 w-full self-start pr-16 text-blue-100 lg:col-span-3">
 			<h1 class="text-5xl font-bold">Adityya Kaushal</h1>
 			<h2 class="pt-4 text-2xl">
 				Mechatronics Undergraduate Student at the University of Waterloo
 			</h2>
-			<h3 class="pr-64 pt-5 text-lg opacity-75">
+			<h3 class="pr-32 pt-5 text-lg opacity-75">
 				I like to explore the intersection of software and hardware!
 			</h3>
 		</div>
-		<div class="w-full">
-			<p class="text-lg leading-loose text-blue-200 opacity-75">
-				I am a university undergraduate student about the intersection between software and
-				hardware, particularily in the field of <b class="text-blue-50 opacity-100">robotics</b>. It
-				is my hope that I can augment great software like artificial intelligence with great
-				hardware like robots.
-			</p>
-			<p class="pt-4 text-lg leading-loose text-blue-200 opacity-75">
-				I am a university undergraduate student about the intersection between software and
-				hardware, particularily in the field of <b class="text-blue-50 opacity-100">robotics</b>. It
-				is my hope that I can augment great software like artificial intelligence with great
-				hardware like robots.
-			</p>
-			<Project />
-			<Project />
+		<div class="w-full lg:col-span-4">
+			<div class="px-4 pb-4 text-lg leading-loose text-blue-200 opacity-50">
+				<p>
+					I am a university undergraduate student about the intersection between software and
+					hardware, particularily in the field of <b class="text-blue-50 opacity-100">robotics</b>.
+					It is my hope that I can augment great software like artificial intelligence with great
+					hardware like robots.
+				</p>
+				<p class="pt-3">
+					I am a university undergraduate student about the intersection between software and
+					hardware, particularily in the field of <b class="text-blue-50 opacity-100">robotics</b>.
+					It is my hope that I can augment great software like artificial intelligence with great
+					hardware like robots.
+				</p>
+			</div>
+			<Project
+				title="Title"
+				body="Hallo I had the very best of the best education"
+				year="2021-2022"
+			/>
+			<Project
+				title="Title"
+				body="Hallo I had the very best of the best education"
+				year="2021-2022"
+			/>
 		</div>
 	</div>
 </div>
