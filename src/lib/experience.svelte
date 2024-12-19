cb3d0771652030cb1c4868562ed92e804b4e0204
Adityya Kaushal <adityyakaushal@gmail.com>
2024-12-18 19:12:00 -0500

    refactored and added test image

--- src/lib/experience.svelte
+++ src/lib/experience.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-	let { src, body, title } = $props();
+	let { year, body, title } = $props();
 </script>
 
 <div
 	class="grid w-full grid-cols-4 rounded-md border-solid px-4 py-4 text-blue-200 hover:bg-blue-300 hover:bg-opacity-5 hover:text-blue-400"
 >
-	<div class="col-span-1"></div>
+	<div class="col-span-1">
+		<h3 class="text-base text-blue-300 opacity-75">{year}</h3>
+	</div>
 	<div class="col-span-3">
 		<h1 class="text-xl font-bold">{title}</h1>
 		<h2 class="text-base text-blue-200 opacity-75 hover:text-blue-200">{body}</h2>
