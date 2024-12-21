ee6227b263bf42e8882b08d5c9b9d65abe969673
Adityya Kaushal <adityyakaushal@gmail.com>
2024-12-21 18:24:03 -0500

    added mobile support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import image1 from '$lib/test.jpg';
 	let m = $state({ x: 0, y: 0 });
 	let y = $state(0);
+	let width = $state(0);
 	let circle_size = $state(1000);
 	function handleMousemove(event: MouseEvent) {
 		m.x = event.clientX;
@@ -12,7 +13,7 @@
 	}
 </script>
 
-<svelte:window bind:scrollY={y} />
+<svelte:window bind:scrollY={y} bind:innerWidth={width} />
 
 <div
 	style="transform:translateX({m.x - circle_size * 0.5}px) translateY({m.y -
@@ -24,7 +25,7 @@
 <div class="flex w-screen justify-center" onmousemove={handleMousemove} role="region">
 	<div class="grid w-full max-w-[1200px] pt-32 lg:grid-cols-7">
 		<div
-			class="w-full self-start px-4 pr-16 text-blue-100 lg:sticky lg:top-32 lg:col-span-3 lg:px-0"
+			class="w-full self-start px-4 pr-16 text-blue-100 lg:sticky lg:top-32 lg:col-span-3 xl:px-0"
 		>
 			<h1 class="text-5xl font-bold">Adityya Kaushal</h1>
 			<h2 class="pt-4 text-2xl">
@@ -33,37 +34,64 @@
 			<h3 class="pr-32 pt-5 text-lg opacity-75">
 				I like to explore the intersection of software and hardware!
 			</h3>
-			<div class="py-10">
-				<ul>
-					<a href="#top">
-						<li class={y < 1 ? 'text-xl font-extrabold transition-all' : 'transition-all'}>
-							&mdash;&mdash;&mdash; About Me
-						</li>
-					</a>
-					<a href="#schooling">
-						<li
-							class={y >= 1 && y < 400 ? 'text-xl font-extrabold transition-all' : 'transition-all'}
-						>
-							&mdash;&mdash;&mdash; School and Work Experience
-						</li>
-					</a>
-					<a href="#clubs">
-						<li
-							class={y >= 400 && y < 700
-								? 'text-xl font-extrabold transition-all'
-								: 'transition-all'}
-						>
-							&mdash;&mdash;&mdash; Clubs and Extracirriculars
-						</li>
-					</a>
-					<a href="#projects">
-						<li class={y >= 700 ? 'text-xl font-extrabold transition-all' : 'transition-all'}>
-							&mdash;&mdash;&mdash; Projects
-						</li>
-					</a>
-				</ul>
-			</div>
-			<div class="flex">
+			{#if width > 1024}
+				<div class="py-10">
+					<ul>
+						<a href="#top">
+							<li class={y < 1 ? 'text-xl font-extrabold transition-all' : 'transition-all'}>
+								&mdash;&mdash;&mdash; About Me
+							</li>
+						</a>
+						<a href="#schooling">
+							<li
+								class={y >= 1 && y < 400
+									? 'text-xl font-extrabold transition-all'
+									: 'transition-all'}
+							>
+								&mdash;&mdash;&mdash; School and Work Experience
+							</li>
+						</a>
+						<a href="#clubs">
+							<li
+								class={y >= 400 && y < 700
+									? 'text-xl font-extrabold transition-all'
+									: 'transition-all'}
+							>
+								&mdash;&mdash;&mdash; Clubs and Extracirriculars
+							</li>
+						</a>
+						<a href="#projects">
+							<li class={y >= 700 ? 'text-xl font-extrabold transition-all' : 'transition-all'}>
+								&mdash;&mdash;&mdash; Projects
+							</li>
+						</a>
+					</ul>
+				</div>
+			{:else}
+				<div class="fixed left-0 top-0 z-30 flex w-full bg-[#00172D]">
+					<ul class="flex w-full justify-between p-4 text-center">
+						<a href="#top">
+							<li class={y < 1 ? 'font-extrabold transition-all' : 'transition-all'}>About Me</li>
+						</a>
+						<a href="#schooling-mobile">
+							<li class={y >= 1 && y < 400 ? 'font-extrabold transition-all' : 'transition-all'}>
+								School and Work Experience
+							</li>
+						</a>
+						<a href="#clubs-mobile">
+							<li class={y >= 400 && y < 700 ? 'font-extrabold transition-all' : 'transition-all'}>
+								Clubs and Extracirriculars
+							</li>
+						</a>
+						<a href="#projects">
+							<li class={y >= 700 ? 'font-extrabold transition-all' : 'transition-all'}>
+								Projects
+							</li>
+						</a>
+					</ul>
+				</div>
+			{/if}
+			<div class="flex py-4 lg:py-0">
 				<div class="pr-5">
 					<a href="https://www.linkedin.com/in/adityya-kaushal-183253310/">
 						<Icon icon="mdi:linkedin" height={35} />
@@ -75,7 +103,7 @@
 			</div>
 		</div>
 		<div class="w-full lg:col-span-4">
-			<div class="px-4 pb-4 text-lg leading-loose text-blue-200 opacity-50" id="about">
+			<div class="px-4 pb-4 pt-4 text-lg leading-loose text-blue-200 opacity-50 lg:pt-0" id="about">
 				<p>
 					I am a university undergraduate student about the intersection between software and
 					hardware, particularily in the field of <b class="text-blue-50 opacity-100">robotics</b>.
@@ -89,6 +117,7 @@
 					hardware like robots.
 				</p>
 			</div>
+			<div class="absolute top-[350px]" id="schooling-mobile"></div>
 			<div class="py-4" id="schooling">
 				<hr class="h-px border-blue-200 opacity-50" />
 			</div>
@@ -110,6 +139,7 @@
 				year="2020-2024"
 				link="https://turnerfenton.peelschools.org/ib"
 			/>
+			<div class="absolute top-[650px]" id="clubs-mobile"></div>
 			<div class="py-4" id="clubs">
 				<hr class="h-px border-blue-200 opacity-50" />
 			</div>
