1a5279d715565e48d042845a3cfe695bf99db0cc
Adityya Kaushal <adityyakaushal@gmail.com>
2024-12-21 18:36:37 -0500

    added meta tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,10 @@
 	}
 </script>
 
+<svelte:head>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+</svelte:head>
+
 <svelte:window bind:scrollY={y} bind:innerWidth={width} />
 
 <div
