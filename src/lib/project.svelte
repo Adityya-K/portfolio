53d6ee6446c11c71837c65a54c4ae3223353157b
Adityya Kaushal <adityyakaushal@gmail.com>
2025-01-02 18:17:21 -0500

    fixed hover color of boxes in light mode

--- src/lib/project.svelte
+++ src/lib/project.svelte
@@ -4,7 +4,7 @@
 
 <a href={link} target="_blank">
 	<div
-		class="grid h-36 w-full grid-cols-4 rounded-md border-solid px-4 py-4 text-black hover:bg-blue-300 hover:bg-opacity-5 hover:text-blue-400 dark:text-blue-200"
+		class="grid h-36 w-full grid-cols-4 rounded-md border-solid px-4 py-4 text-black hover:bg-gray-800 hover:bg-opacity-5 hover:text-blue-400 dark:text-blue-200 dark:hover:bg-blue-300"
 	>
 		<div class="col-span-1 pr-4">
 			<img {src} class="h-full w-full rounded-md object-cover" alt="" />
